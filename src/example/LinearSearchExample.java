324580fa3dc0c43e7fb2c97b55d1060e7cdcd29d
Mukta Bhoite <muktabhoite@Muktas-MacBook-Air.local>
2026-07-09 21:16:10 +0530

    Maximum element in array

---
+++ src/example/LinearSearchExample.java
@@ -0,0 +1,27 @@
+package example;
+import java.util.Scanner;
+public class LinearSearchExample {
+	public static int linearSearch(int arr[],int n) {
+		int i;
+		for(i=0;i<arr.length-1;i++) {
+			if(arr[i] == n) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String arg[]) {
+		Scanner sc=new Scanner(System.in);
+		int[] arr= {1,2,3,4,5,6};
+		int n;
+		System.out.println("Enter no to search in dataset : ");
+		n=sc.nextInt();
+		int res=linearSearch(arr,n);
+		if(res == -1) {
+			System.out.println("Element not found");
+		}
+		else {
+			System.out.println("Element found at : "+res);
+		}
+	}
+}
