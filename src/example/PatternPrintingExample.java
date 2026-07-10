e19a696d60ae42e2075d56dd942fdb291bf3c1f8
Mukta Bhoite <muktabhoite@Muktas-MacBook-Air.local>
2026-07-10 21:15:27 +0530

    Pattern printing2

--- src/example/PatternPrintingExample.java
+++ src/example/PatternPrintingExample.java
@@ -5,7 +5,15 @@ public class PatternPrintingExample {
 		int i, j;
 		for(i=1; i<=5; i++) {
 			for(j=1; j<=i; j++) {
-				System.out.println("* ");
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+		for(i=5; i>=1; i--) {
+			for(j=1; j<=i; j++) {
+				System.out.print(" * ");
 			}
 			System.out.println();
 		}
