8a5e7020b8c25c9069109f2041604123ccfe4bb8
Mukta Bhoite <muktabhoite@Muktas-MacBook-Air.local>
2026-07-10 21:20:43 +0530

    Sorting numbers manually

---
+++ src/example/SortNumbersManually.java
@@ -0,0 +1,30 @@
+package example;
+import java.util.Scanner;
+
+public class SortNumbersManually {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers : ");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Numbers:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
